546b8b7c15d6c226a820f750ff8548c8d53eefce
Alexander <a@a>
2024-11-20 16:37:21 +0300

    long numbers

--- src/telran/memory/utils/MemoryService.java
+++ src/telran/memory/utils/MemoryService.java
@@ -4,9 +4,9 @@ public class MemoryService {
     public static int getMaxAvailableMemory() {
         // TODO Homework
         int l = 1;
-        int r = Integer.MAX_VALUE - 1;
+        int r = Integer.MAX_VALUE;
         while (r - l > 1) {
-            int m = (l + r) / 2;
+            int m = (int)(((long)l + r) / 2);
             try {
                 int[] arr = new int[m];
                 l = m;
